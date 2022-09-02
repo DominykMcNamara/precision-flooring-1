18f53627c06baf92d39bb275f09a66f9c96effae
DominykMcNamara <dominyksmith@gmail.com>
2022-09-01 20:52:49 -0400

    Begin services section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Header } from '../components/Header'
+import React from "react";
+import { Header } from "../components/Header";
+import { Services } from "../components/Services";
 
 export const Home = () => {
   return (
-    <header className="w-100">
-      <Header />
-    </header>
-  )
-}
+    <>
+      <header className="w-100">
+        <Header />
+      </header>
+      <section>
+        <Services />
+      </section>
+    </>
+  );
+};
