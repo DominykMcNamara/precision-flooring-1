3c53f4e2a47d27e33bf4f84464891093be9c72d0
DominykMcNamara <dominyksmith@gmail.com>
2022-09-04 20:28:42 -0400

    Update Services component classNames

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,20 +3,22 @@ import BirchFloor from "../assets/markus-spiske-3tbtq3oCQ1s-unsplash.jpg";
 
 export const Services = () => {
   return (
-    <div className=" w-[75rem] bg-stone-600 ml-[25vw] flex flex-row justify-center align-center flex-row pt-[2rem] pb-[2rem] pl-[5rem] pr-[5rem] mt-[3rem] rounded">
+    <div className="w-screen">
+    <div className=" w-1/2 bg-stone-600 mx-auto flex flex-row justify-center align-center flex-row p-10 mt-20 rounded">
       <img
         src={BirchFloor}
         alt="Birchwood Floor"
-        className="w-[60rem] h-[20rem] rounded"
+        className="w-2/4 h-15 rounded"
       />
-      <div className=" ml-[5rem] text-center text-stone-200">
-        <h1 className="text-5xl text-orange-600 mt-[2.5rem]">Services</h1>
-        <h3 className="w-[30rem] mt-[1rem] text-2xl font-regular">
+      <div className=" ml-3 text-center text-stone-200">
+        <h1 className="text-5xl text-orange-600 mt-10">Services</h1>
+        <h3 className=" mt-2 text-2xl font-regular">
           We provide high quality flooring in a vaiety of categories including
           hardwood, carpet, vinyl, tile. laminate, and more in residential &
           commercial applications
         </h3>
       </div>
+    </div>
     </div>
   );
 };
