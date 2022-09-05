0d5852e6fb538769e4b0e0ac98a90be7520b917c
DominykMcNamara <dominyksmith@gmail.com>
2022-09-04 21:04:25 -0400

    styled contactForm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export const ContactForm = () => {
   return (
-    <div>ContactForm</div>
-  )
-}
+    <div className="w-screen mt-10 rounded ">
+      <form className="flex bg-stone-600 flex-col mx-auto text-3xl w-1/2 p-10">
+        <h1 className="text-center text-5xl text-orange-600">Contact</h1>
+        <label className="flex flex-col mx-auto p-3 text-stone-200 text-2xl">
+          Name:
+          <input
+            type="text"
+            className="form-input w-96 mt-2 text-stone-800"
+            required
+          ></input>
+        </label>
+        <label className="flex flex-col mx-auto  p-3 text-stone-200 text-2xl">
+          Email:
+          <input
+            type="email"
+            className="form-input w-96 mt-2 text-stone-800"
+            required
+          ></input>
+        </label>
+        <label className="flex flex-col mx-auto  p-3 mt-2 text-stone-200 text-2xl">
+          How may we help you?
+          <textarea className="form-textarea w-96 text-stone-800" />
+        </label>
+        <button className="bg-orange-500 mx-auto text-stone-300 w-96 mt-6 p-1 text-2xl rounded">
+          SUBMIT
+        </button>
+      </form>
+    </div>
+  );
+};
