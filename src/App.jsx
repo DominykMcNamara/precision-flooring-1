2ee2410fe95bb0d6a6e39e771815f34f6877df2b
DominykMcNamara <dominyksmith@gmail.com>
2022-09-12 15:29:26 -0400

    implement context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import { Home } from "./routes/Home";
+import { Home } from "./routes/Home"
 import { Login } from "./routes/Login";
+import { AuthProvider } from "./context/AuthContext";
+
+
 
 export const App = () => {
-  return (
-    <AuthProvider>
-    <div>
-      <Router>
-        
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/" element={<Login />} />
-          </Routes>
-        
-      </Router>
-    </div>
-    </AuthProvider>
-  );
-};
+    return (
+      <AuthProvider>
+        <div>
+          <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/login" element={ <Login />} />
+            </Routes>
+          </Router>  
+        </div>
+        </AuthProvider>
+    )
+}
